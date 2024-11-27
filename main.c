8976466a016288c4a135d683bbdc4e947ac1a96c
sombra <analogsombra@gmail.com>
2024-11-27 08:16:14 +0530

    build: raylib added

--- main.c
+++ main.c
@@ -1,7 +1,34 @@
 #include <stdlib.h>
 #include "console.h"
+#include "raylib.h"
+
+void UpdateDrawFrame(void);
 int main()
 {
+    int screenWidth = 800;
+    int screenHeight = 450;
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        UpdateDrawFrame();
+    }
+
+    CloseWindow();
+
     showname();
     return EXIT_SUCCESS;
+}
+void UpdateDrawFrame(void)
+{
+
+    BeginDrawing();
+
+    ClearBackground(RAYWHITE);
+
+    DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
+
+    EndDrawing();
+    //----------------------------------------------------------------------------------
 }
